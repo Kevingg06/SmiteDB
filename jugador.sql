69cb4758a3de330037aea605f629115ee7597f9b
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2023-10-23 17:38:18 -0300

    Inserts de jugador
    Subo los inserts de la tabla jugador completos

---
+++ jugador.sql
@@ -0,0 +1,11 @@
+insert into jugador(nombre, personaje)
+    values  ("Kevin Gonzalez", "Bacchus"),
+            ("Damian Maldonado", "Aquiles"),
+            ("Martín Galeano", "Anubis"),
+            ("Agustín Oliverio", "Kukulkan"),
+            ("Ezequiel Fernandez", "Cerbero"),
+            ("Nicolás Lopez", "Kumbhakarna"),
+            ("Valentín Cañete", "Afrodita"),
+            ("Benjamín Castro Madrid", "Maui"),
+            ("Agustina Gonzalez", "Ratatosk"),
+            ("Matheo Moro", "Baba Yaga");
