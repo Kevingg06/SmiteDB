48ff16153113c61f3b400c2712de185c846276a6
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2023-11-02 17:28:52 -0300

    Inserts panteon
    Subo los inserts de la tabla "panteon" completos

---
+++ panteon.sql
@@ -0,0 +1,17 @@
+insert into panteon(panteon)
+        values  ("Arturiano"),
+                ("Babilónico"),
+                ("Chino"),
+                ("Celta"),
+                ("Egipcio"),
+                ("Griego"),
+                ("Grandes Antiguos"),
+                ("Hindú"),
+                ("Japonés"),
+                ("Maya"),
+                ("Nórdico"),
+                ("Polinesio"),
+                ("Romano"),
+                ("Eslavo"),
+                ("Vudú"),
+                ("Yoruba");
